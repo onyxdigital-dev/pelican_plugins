2d40797909535bc957a2326e90009efbf3e6e6cf
X_olli <97686132+olligatorugef@users.noreply.github.com>
2026-01-14 17:37:00 +0100

    Add system-status-monitor/lang/fr-FR/messages.php

---
+++ system-status-monitor/lang/fr-FR/messages.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+    'navigation_label' => 'État Système',
+
+    'titles' => [
+        'cpu_usage' => 'Utilisation CPU',
+        'cpu_details' => 'Détails CPU',
+        'memory_usage' => 'Utilisation Mémoire',
+        'disk_usage' => 'Utilisation Disque',
+        'load_average' => 'Charge Moyenne',
+        'system_info' => 'Informations Système',
+        'uptime' => 'Temps de Fonctionnement',
+        'performance' => 'Performance',
+        'process_info' => 'Informations Processus',
+    ],
+
+    'labels' => [
+        'os' => 'Système d\'Exploitation',
+        'php_version' => 'Version PHP',
+        'cores' => 'Cœurs',
+        'threads' => 'Threads',
+        'cpu_model' => 'Modèle CPU',
+        'swap_memory' => 'Mémoire Virtuelle',
+        'processes' => 'Processus',
+        'running' => 'En Exécution',
+        'idle' => 'Inactif',
+        'last_reboot' => 'Dernier Redémarrage',
+        'mounted_points' => 'Points Montés',
+        'inode_usage' => 'Utilisation Inode',
+        'memory' => 'Mémoire',
+        'used' => 'Utilisée',
+        'total' => 'Totale',
+        'free' => 'Libre',
+        'one_min' => '1 minute',
+        'five_min' => '5 minutes',
+        'fifteen_min' => '15 minutes',
+    ],
+
+    'status' => [
+        'excellent' => 'Excellent',
+        'good' => 'Bon',
+        'warning' => 'Attention',
+        'critical' => 'Critique',
+        'online' => 'En Ligne',
+        'unavailable' => 'Non Disponible',
+    ],
+];
